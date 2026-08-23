7b9b2a6dc5f1e8bf10400fc7bc7897bfa460aa21
Muhammad Aayan Shaikh <muhammadaayanshaikhforbusiness@gmail.com>
2026-08-23 15:43:43 +0500

    folder-1 added and some changes

---
+++ folder-1/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div>Hello World!</div>
+  )
+}
+
+export default App
+
+
+// function App() {
+//   return <h1>Hello World!</h1>
+// }
+
+// export default App
