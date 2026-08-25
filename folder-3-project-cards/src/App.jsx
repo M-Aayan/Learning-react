d2cbe8a1529e4ec0a13701325e819245497a01db
Muhammad Aayan Shaikh <muhammadaayanshaikhforbusiness@gmail.com>
2026-08-26 02:09:02 +0500

    make card project

---
+++ folder-3-project-cards/src/App.jsx
@@ -0,0 +1,12 @@
+import Cards from "./components/Cards"
+
+const App = () => {
+  return (
+    <div className="parent">
+      <Cards />
+      <Cards />
+    </div>
+  )
+}
+
+export default App
---
+++ folder-3-project-cards/src/components/Cards.jsx
@@ -0,0 +1,25 @@
+
+
+const Cards = () => {
+    return (
+        <div className="Card">
+            <div className="card-top">
+                <p>GBD78865JS</p>
+                <p>24/08/2026</p>
+            </div>
+            <div className="card-content">
+                <p>Infinity Event Organizer</p>
+                <div class="title-row">
+                    <h2>Summer Music Festival</h2>
+                    <button>Upcoming</button>
+                </div>
+            </div>
+            <div className="card-bottom">
+                <h4>$65</h4>
+                <button>View Details</button>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
